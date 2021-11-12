7da7572b476881e082376e24ed0426251c5c20c3
Enes.Aydin <enes.aydin@shbndn-mc-05-34-EnesAydin.local>
2021-11-12 14:12:44 +0300

    update

--- hackathon-core/src/main/java/com/hackathon/annotation/Driver.java
+++ hackathon-core/src/main/java/com/hackathon/annotation/Driver.java
@@ -1,5 +1,15 @@
 package com.hackathon.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface Driver
 {
+
 }
