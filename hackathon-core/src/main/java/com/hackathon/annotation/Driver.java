55495a83a304940094bf7422e690cf9363397f6b
Amil Uslu <Amil.Uslu@sahibinden.com>
2021-11-12 11:03:25 +0300

    Driver Annotation updated

---
+++ hackathon-core/src/main/java/com/hackathon/annotation/Driver.java
@@ -0,0 +1,5 @@
+package com.hackathon.annotation;
+
+public @interface Driver
+{
+}
---
+++ hackathon-core/src/main/java/com/hackathon/extensions/AppiumExtension.java
@@ -0,0 +1,15 @@
+package com.hackathon.extensions;
+
+import com.hackathon.driver.AppiumDriverManager;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class AppiumExtension
+{
+    protected static final ExtensionContext.Namespace EXTENSION_NAMESPACE = ExtensionContext.Namespace.create(AppiumExtension.class);
+
+    private static final Class<Driver> DRIVER_CLASS = Driver.class;
+    protected static final String SESSION = "session";
+    protected AppiumDriverManager appiumDriverManager;
+
+
+}
