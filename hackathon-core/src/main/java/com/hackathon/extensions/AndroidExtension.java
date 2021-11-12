7c6a95e087bd86823a4451665fb656234560b90b
Amil Uslu <Amil.Uslu@sahibinden.com>
2021-11-12 10:40:18 +0300

    Junit5 Android Extension addded

---
+++ hackathon-core/src/main/java/com/hackathon/extensions/AndroidExtension.java
@@ -0,0 +1,23 @@
+package com.hackathon.extensions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+@Slf4j
+public class AndroidExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback
+{
+
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext)
+    {
+        log.info("Test Finished : {}#{}", extensionContext.getTestClass().get().getSimpleName(), extensionContext.getTestMethod().get().getName());
+    }
+
+    @Override
+    public void beforeTestExecution(ExtensionContext extensionContext)
+    {
+        log.info("Test Started : {}#{}", extensionContext.getTestClass().get().getSimpleName(), extensionContext.getTestMethod().get().getName());
+    }
+}
