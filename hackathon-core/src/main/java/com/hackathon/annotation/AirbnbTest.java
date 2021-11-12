b808750b3d9b22c69319a34ba33e3a2d3a67636d
nahide.ergun <nahide.ergun@sahibinden.com>
2021-11-12 12:22:25 +0300

    airbnb test annotation added

---
+++ hackathon-core/src/main/java/com/hackathon/annotation/AirbnbTest.java
@@ -0,0 +1,25 @@
+package com.hackathon.annotation;
+
+import com.hackathon.extensions.AndroidExtension;
+import com.hackathon.extensions.ExtentReportExtension;
+import com.hackathon.provider.CrossDeviceProvider;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Execution(ExecutionMode.CONCURRENT)
+@ExtendWith({AndroidExtension.class, ExtentReportExtension.class})
+@ParameterizedTest
+@ArgumentsSource(CrossDeviceProvider.class)
+public @interface AirbnbTest
+{
+}
