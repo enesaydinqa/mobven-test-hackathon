af4fe624efd819a959cefb87ca9fed487c748199
nahide.ergun <nahide.ergun@sahibinden.com>
2021-11-12 11:57:43 +0300

    resources and properties added

---
+++ hackathon-core/src/main/java/com/hackathon/properties/DriverProp.java
@@ -0,0 +1,50 @@
+package com.hackathon.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@Configuration
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:driver.properties")
+public class DriverProp
+{
+    public static DriverProp driverProp;
+
+    @Autowired
+    public void DriverProp(DriverProp driverProp)
+    {
+        DriverProp.driverProp = driverProp;
+    }
+
+    @Value("#{systemProperties['android.home'] ?: systemProperties['user.home']+'/Library/Android/sdk'}")
+    public String androidHomePath;
+
+    @Value("#{systemProperties['node.path'] ?: '/usr/local/bin/node'}")
+    public String nodePath;
+
+    @Value("#{systemProperties['appium.path'] ?: '/usr/local/bin/appium'}")
+    public String appiumPath;
+
+    @Value("#{systemProperties['appium.driver.local.service'] ?: false}")
+    public Boolean appiumDriverLocalService;
+
+    @Value("#{systemProperties['user.dir']+'${apk.file.path}'}")
+    public String apkFilePath;
+
+    @Value("#{systemProperties['user.dir']+'${debug.keystore}'}")
+    public String debugKeyStore;
+
+    @Value("${app.package}")
+    public String appPackage;
+
+    @Value("${app.activity}")
+    public String appActivity;
+
+    public static DriverProp getDriverProp()
+    {
+        return driverProp;
+    }
+}
