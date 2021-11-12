96ace3c79e740abe0940d6e7e688c8d950748af0
Enes.Aydin <enes.aydin@shbndn-mc-05-34-EnesAydin.local>
2021-11-12 10:38:41 +0300

    appiumdrivermanager

--- hackathon-core/src/main/java/com/hackathon/AppiumDriverManager.java
+++
@@ -1,5 +0,0 @@
-package com.hackathon;
-
-public class AppiumDriverManager
-{
-}
---
+++ hackathon-core/src/main/java/com/hackathon/driver/AppiumDriverManager.java
@@ -0,0 +1,19 @@
+package com.hackathon.driver;
+
+import io.appium.java_client.AppiumDriver;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@Slf4j
+public class AppiumDriverManager
+{
+    public final AppiumDriver driver;
+    private final WebDriverWait webDriverWait;
+    private static final int DEFAULT_TIMEOUT = 10;
+
+    public AppiumDriverManager(AppiumDriver appiumDriver)
+    {
+        this.driver = appiumDriver;
+        this.webDriverWait = new WebDriverWait(driver, DEFAULT_TIMEOUT);
+    }
+}
